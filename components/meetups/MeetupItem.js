3debee0241f9b6584c97ed6ff25789b1819dbad5
mazen <mazenadel19@gmail.com>
2021-08-01 20:06:51 +0200

    replacing link with programmatic naviagion and creating meetup detail comp

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,24 +1,29 @@
-import Link from 'next/link';
-import Card from '../ui/Card';
-import classes from './MeetupItem.module.css';
+import { useRouter } from 'next/dist/client/router'
+import Card from '../ui/Card'
+import classes from './MeetupItem.module.css'
 
 function MeetupItem(props) {
-  return (
-    <li className={classes.item}>
-      <Card>
-        <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
-        </div>
-        <div className={classes.content}>
-          <h3>{props.title}</h3>
-          <address>{props.address}</address>
-        </div>
-        <div className={classes.actions}>
-          <Link href='/'>Show Details</Link>
-        </div>
-      </Card>
-    </li>
-  );
+	const router = useRouter()
+	const showDetailHandler = () => {
+		router.push(`/` + props.id)
+	}
+
+	return (
+		<li className={classes.item}>
+			<Card>
+				<div className={classes.image}>
+					<img src={props.image} alt={props.title} />
+				</div>
+				<div className={classes.content}>
+					<h3>{props.title}</h3>
+					<address>{props.address}</address>
+				</div>
+				<div className={classes.actions}>
+					<button onClick={showDetailHandler}>Show Details</button>
+				</div>
+			</Card>
+		</li>
+	)
 }
 
-export default MeetupItem;
+export default MeetupItem
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,10 +1,13 @@
-import { useRouter } from 'next/dist/client/router'
-
+import MeetupDetail from '../../components/meetups/MeetupDetail'
 const MeetupPage = () => {
-	const router = useRouter()
-	const meetupId = router.query.meetupId
-
-	return <>{meetupId}</>
+	return (
+		<MeetupDetail
+			image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
+			title='First Meetup'
+			address='Some Street 5, Some City'
+			description='This is a first meetup'
+		/>
+	)
 }
 
 export default MeetupPage
